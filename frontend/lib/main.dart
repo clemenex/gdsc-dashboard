1eee54ed599c528d5a7ba7f88f883efb942bdd8e
clemenex <clemenexcortex@gmail.com>
2025-01-11 21:16:05 +0800

    chore: fix dotenv issues

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
+
+  try {
+    await dotenv.load();
+    print('Environment variables loaded successfully');
+    print(dotenv.env['API_KEY']);
+  } catch (e) {
+    print('Error loading .env file: $e');
+  }
+
   runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
